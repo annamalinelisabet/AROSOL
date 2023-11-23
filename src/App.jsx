0bd9e6666ba5b3ad10489370c37b61d702f91fd5
Malin Andersson <annamalinelisabet@gmail.com>
2023-11-23 14:30:54 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Views from './views/Views'
+
+function App() {
+
+  return (
+    <>
+      <Views/>
+    </>
+  )
+}
+
+export default App
---
+++ src/views/Views.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import HomeView from './Home/HomeView'
+
+const Views = () => {
+  return (
+    <Routes>
+        <Route path='/' element={ <HomeView/> }/>
+    </Routes>
+  )
+}
+
+export default Views
