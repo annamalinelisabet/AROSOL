3896a4f49b89b268939b7cb12275256d63b673db
Malin Andersson <annamalinelisabet@gmail.com>
2024-01-09 10:21:57 +0100

    About page and contact section mm

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,27 +2,23 @@ import React from 'react'
 import './Button.css'
 
 const Button = ({
-    align = '',
     onClick,
     text = '',
     variant = '',
 }) => {
-    const btnWrapper = 'btnWrapper'
     const btn = 'btn'
 
   return (
     <div className='Button'>
-        <div className={` ${btnWrapper} ${align === 'left' ? 'flex-start' :  align === 'right' ? 'flex-end' : 'flex-center'}`}>
-            <button
-            onClick={(data) => onClick(data)}
-            className={`${btn} ${
-                variant === "light"
-                ? 'btnLight'
-                : 'btnDark'
-            }`}>
-            {text}
-            </button>
-        </div>
+        <button
+        onClick={(data) => onClick(data)}
+        className={`${btn} ${
+            variant === 'light'
+            ? 'btnLight'
+            : 'btnDark'
+        }`}>
+        {text}
+        </button>
     </div>
   )
 }
--- src/views/Solel/SolelView.jsx
+++ src/views/Solel/SolelView.jsx
@@ -1,11 +1,14 @@
 import './SolelView.css'
 import { useEffect } from 'react';
-import Image from '../../assets/bild.jpg'
+import HeroImage from '../../assets/solelhero.png'
+import Image from '../../assets/montering.png'
+import WinterImage from '../../assets/alperna.png'
 import PageHero from '../../components/PageHero/PageHero';
 import Button from '../../components/Button/Button';
 import { IoSunnyOutline } from "react-icons/io5";
 import { BsFillLightningChargeFill } from "react-icons/bs";
 import { PiCoins } from "react-icons/pi";
+import ContactSection from '../../components/ContactSection/ContactSection';
 
 const SolelView = () => {
 
@@ -13,22 +16,23 @@ const SolelView = () => {
     window.scrollTo(0, 0)
   }, [])
 
-  const infoText = 'Att investera i solel med Arosol är inte bara ett miljövänligt val, det är även ekonomiskt smart och strategiskt. Genom att använda våra solpaneler blir ditt hem eller företag inte bara mer värdefullt,  utan du sticker också ut från konkurrenterna genom tydligt miljöengagemang. Dessutom öppnar investeringen i våra solenergilösningar upp möjligheter till skatteförmåner och deltagande i olika incitamentprogram. Detta innebär att utöver de omedelbara ekonomiska fördelarna kan du också dra nytta av ytterligare stöd och incitament som kan göra din övergång till solenergi ännu mer fördelaktig. Ta det första steget mot en hållbar framtid med Arosol.'
+  const infoText1 = 'Att investera i solel med Arosol är inte bara ett miljövänligt val, det är även ekonomiskt smart och strategiskt. Genom att använda våra solpaneler blir ditt hem eller företag inte bara mer värdefullt, utan du sticker också ut från konkurrenterna genom tydligt miljöengagemang.'
+  const infoText2 = 'Dessutom öppnar investeringen i våra solenergilösningar upp möjligheter till skatteförmåner och deltagande i olika incitamentprogram. Detta innebär att utöver de omedelbara ekonomiska fördelarna kan du också dra nytta av ytterligare stöd och incitament som kan göra din övergång till solenergi ännu mer fördelaktig. Ta det första steget mot en hållbar framtid med Arosol.'
 
   return (
     <div className='Solel'>
 
-      <PageHero alt='Solceller' image={Image} title='Solel' />
+      <PageHero alt='Solceller' image={HeroImage} title='Solel' />
 
       <div className='info-wrapper'>
         <div className='container'>
           <div className='text-box'>
             <h1 className='subtitle'>EN SMART INVESTERING</h1>
-            <p className='mobile-text'>{infoText}</p>
+            <p className='mobile-text'>{infoText1}<span className='line-break'>{infoText2}</span></p>
             <p className='text'>Arosol installerar högkvalitativa solcellspaneler för optimal avkastning.</p>
             <Button text='Få offert' />
           </div>
-          <p className='text'>{infoText}</p>
+          <p className='text'>{infoText1}<span className='line-break'>{infoText2}</span></p>
         </div>
       </div>
 
@@ -58,7 +62,20 @@ const SolelView = () => {
         </div>
       </div>
 
+      <div className='winter-info-wrapper'>
+        <div className='winter-img-wrapper'>
+          <img src={WinterImage} alt='Pekar på snöiga bergstoppar' />
+        </div>
+        <div className='winter-info'>
+          <h2 className='container'>Året runt energi: Solpanelernas beständighet i vinterkylan</h2>
+          <p className='container'>Trots kortare dagar och kallt väder under vintern fortsätter solpanelerna att generera värdefull energi. Det är faktiskt så att solcellerna producerar elektricitet även under en molnig vinterdag och fungerar utmärkt i vårt nordliga klimat. Solcellerna vi på Arosol använder oss av är designade för att enkelt släppa ifrån sig snö, vilket gör dem robusta och anpassningsbara till olika väderförhållanden. Detta säkerställer en konstant och pålitlig energiproduktion året runt.</p>
+          <br />
+          <p className='container'>Under sommartid ökar solpanelernas energiproduktion vilket kan resultera i överskottsel. Denna överskottsel kan användas för att minska eller helt eliminera elkostnaderna under sommaren. Besparingarna från soliga månader kan agera som en ekonomisk buffert och användas för att täcka de högre elkostnaderna som vanligtvis uppstår under vintermånaderna</p>
+        </div>
+      </div>
       
+      <ContactSection header='Vill ni veta mer?' text='Är ni nyfikna på solpaneler och vill lära er mer om deras montering, funktion och andra aspekter? Tveka inte att kontakta oss! På Arosol är vi här för att svara på era frågor och erbjuda all information ni behöver för att fatta välgrundade beslut om solenergilösningar. Kontakta oss och låt oss guida er mot en mer hållbar energiframtid.' />
+
     </div>
   )
 }
