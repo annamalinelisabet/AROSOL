2fd510f3c583d8768c496d5813456c53beb8c96a
Malin Andersson <annamalinelisabet@gmail.com>
2024-03-13 19:14:24 +0100

    Change small details

--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -7,8 +7,6 @@ const ReviewCard = ({ text, img, name, company, alt, instagramLink }) => {
     <div className='ReviewCard'>
       <FaQuoteLeft className='quote-icon' />
       <p className='text'>{text}</p>
-        {/* <a href='https://www.instagram.com/p/CwBVifNNmkS/?img_index=1' target='_blank' rel='noopener noreferrer' aria-label='Link to Instagram' className='view-link' >Klicka här för att se resultat</a> */}
-        <a href={instagramLink} target='_blank' rel='noopener noreferrer' aria-label='Link to Instagram' className='view-link' ><FaRegImages className='view-icon'/></a>
       <div className='person-wrapper'>
         <div className='quote-img-wrapper'>
           <img src={img} alt={alt} />
