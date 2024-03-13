246a894a5e8e421fc93ae3b1a26152a113f60dc1
Anna <annaelg@live.se>
2024-03-13 16:05:08 +0100

    Add link to img in review card

--- src/views/Home/HomeView.jsx
+++ src/views/Home/HomeView.jsx
@@ -128,7 +128,7 @@ const HomeView = () => {
       <div className='review-section'>
         <h2 className='sub-header'>Vad tycker våra kunder?</h2>
         <swiper-container ref={swiperRef} init='false'>
-          <swiper-slide><ReviewCard company='Privatperson' img={Review1} name='Lennart Pettersson' text='Jag upplevde kvalitet och professionalism med Arosol! Hos Arosol finns ingen kompromiss när det gäller kvalitet - deras fokus är att leverera utmärkt resultat. Jag är helt övertygad om att jag valde rätt när jag anlitade dem för ett komplett takbyte på altanen.' /></swiper-slide>
+          <swiper-slide><ReviewCard company='Privatperson' img={Review1} name='Lennart Pettersson' text='Med Arosol fick jag toppkvalitet och professionalism utan kompromisser. Resultatet är fantastiskt. Jag har inga tvivel om att jag valde rätt när jag anlitade dem för ett komplett takbyte på altanen.'  instagramLink='https://www.instagram.com/p/CwBVifNNmkS/?img_index=1'/></swiper-slide>
           <swiper-slide><ReviewCard company='VD Hallstahem' img={Review2} name='Emma Josefsson' text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, architecto.' /></swiper-slide>
           <swiper-slide><ReviewCard company='Privatperson' img={Review2} name='Gunnar Gunnarson Hej' text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, architecto.' /></swiper-slide>
 -        </swiper-container>       
