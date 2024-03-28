515add212bf6acd7d9eb7ccb5a8fbcef69e98779
Anna <annaelg@live.se>
2024-03-28 14:39:32 +0100

    Improve text on Om oss

--- src/views/About/AboutView.jsx
+++ src/views/About/AboutView.jsx
@@ -30,7 +30,7 @@ const AboutView = () => {
           <div className='container'>
             {/* ÄNDRA STORLEK SAMMA SOM KONTAKT ?  */}
             <h1 className='about-header'>Vi är Arosol</h1> 
-            <p>Arosol är din dedikerade partner för hållbara lösningar. Vi erbjuder en mångsidig tjänstepalett inom solel, plåtslageri, taksäkerhet och takentreprenad. Specialiserade inom områdena solel, taksäkerhet, takentreprenad och plåtslageri, strävar vi efter att erbjuda skräddarsydda och högkvalitativa tjänster. Med vår dedikerade och kunniga personal arbetar vi nära våra kunder för att leverera energieffektiva solenergilösningar, säkra takkonstruktioner och läckra plåtarbeten. <span className='new-paragraph'> Vårt fokus på kvalitet och professionalism gör Arosol till det självklara valet för alla dina takrelaterade behov, inklusive specialkompetens inom plåtarbeten. Oavsett om det gäller solpaneler, plåtarbeten eller takprojekt kan du lita på Arosol för professionell och pålitlig service.</span></p>
+            <p>Arosol är din dedikerade partner. Vi erbjuder en mångsidig tjänstepalett inom solel, plåtslageri, taksäkerhet och takentreprenad. Vi strävar efter att erbjuda skräddarsydda och högkvalitativa tjänster. Vi arbetar nära våra kunder för att leverera energieffektiva solenergilösningar, säkra takkonstruktioner och läckra plåtarbeten. <span className='new-paragraph'> Vårt fokus på kvalitet och professionalism gör Arosol till det självklara valet. Du kan lita på Arosol oasvett om det gäller solpaneler, plåtarbeten eller takprojekt.</span></p>
             <div className='mt-2'>
               <a href='/contact'><Button text='Kontakta oss'/></a>
             </div>
@@ -40,10 +40,10 @@ const AboutView = () => {
         <div className='card-section container'>
             <h2 className='sub-header'>Träffa teamet</h2>
           <div className='card-wrapper'>
-            <StaffCard image={Pontus} name='Pontus Bergman' text='Vår VD och grundare är expert inom plåt, solel, tak och säkerhet. Han kan det mesta och lär sig resten. Älskar att vara aktiv, oavsett om det är snowboard i fjällen eller mountainbike i skogen.' />
-            <StaffCard image={Stefan} name='Stefan Nilsson' text='Vår grundare och tekniska supportguru älskar sitt arbete. På fritiden njuter han av paragliding och utforskar teknikvärldens möjligheter.' />
-            <StaffCard image={Mikael} name='Mikael Syrén' text='Med över 20 års erfarenhet i plåtbranschen, kan man lugnt säga att Mikael är en trygg och pålitlig expert. På fritiden älskar han att fiska och spendera tid med sina fyrbenta vänner.' />
-            <StaffCard image={Albin} name='Albin Sundnäs' text='Utbildad plåtslagare och solelsmontör. Han brinner för träning och hälsa. För att varva ner söker han sig gärna till vattnet för en dag med fiske.' />
+            <StaffCard image={Pontus} name='Pontus Bergman' text='Vår vd och grundare är expert inom plåt, solel, tak och säkerhet. Han kan det mesta och lär sig resten. Han är gärna aktiv, oavsett om det är snowboard i fjällen eller mountainbike i skogen.' />
+            <StaffCard image={Stefan} name='Stefan Nilsson' text='Stefan är Arosols andra grundare och tekniska supportguru. På fritiden njuter han av paragliding och utforskar teknikvärldens möjligheter.' />
+            <StaffCard image={Mikael} name='Mikael Syrén' text='Med över 20 års erfarenhet i plåtbranschen kan man lugnt säga att Mikael är en trygg och pålitlig expert. På fritiden älskar han att fiska och spendera tid med sina fyrbenta vänner.' />
+            <StaffCard image={Albin} name='Albin Sundnäs' text='Albin är utbildad plåtslagare och solelsmontör med en passion för träning och hälsa. För att varva ner söker han sig gärna till vattnet för en dag med fiske.' />
             <StaffCard image={Michael} name='Michael Karlsson' text='Vår hyllade plåtveteran har inte bara bemästrat sitt yrke utan förvandlar även sitt sommarställe till en skönhet. På sommaren njuter han av båtturer och naturen.' />
           </div>
         </div>
