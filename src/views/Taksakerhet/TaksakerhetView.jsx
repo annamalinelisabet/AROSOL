9e140cfb78d389610e09709ba05dc03f689102e3
Anna <annaelg@live.se>
2024-03-28 13:34:06 +0100

    Improve and modify text on Taksäkerhet

--- src/views/Taksakerhet/TaksakerhetView.jsx
+++ src/views/Taksakerhet/TaksakerhetView.jsx
@@ -41,19 +41,19 @@ const TaksakerhetView = () => {
         header='Säkert tak med Arosol'
         text='Din erfarna partner för taksäkerhet och långsiktig trygghet'
         infoText1='Arosol är din pålitliga partner för taksäkerhet av en enkel anledning: vårt hängivna engagemang för att leverera skräddarsydda och tillförlitliga lösningar som säkrar och stärker ditt tak.'
-        infoText2='Arosol sätter en ny standard inom taksäkerhet genom att kombinera teknisk expertis med personlig service. Vi förstår att varje tak är unikt och anpassar våra lösningar efter dina specifika behov. Vårt engagerade team sträcker sig längre för att säkerställa att varje detalj hanteras med noggrannhet och omsorg'
+        infoText2='Arosol sätter en standard inom taksäkerhet genom att kombinera teknisk expertis med personlig service. Vi förstår att varje tak är unikt och anpassar våra lösningar efter dina specifika behov. Vårt engagerade team säkerställa att varje detalj hanteras med noggrannhet och omsorg.'
         btnText='Kontakta oss'
         link='/contact'
       />
 
     <div className='safety-wrapper'>
       <div className='safety-section container'>
-        <h2 className='sub-header'>Takskyddstjänster för varje säsong</h2>
+        <h2 className='sub-header'>Taktjänster för varje säsong</h2>
         <div className='safety-card-wrapper'>
           <div className='safety-card'>
             <div><MdOutlineHandyman className='safety-icon' /></div>
             <h3>Snörasskydd och dräneringssystem</h3>
-            <p>Installation av snörasskydd för att förhindra snöskador och isbildning, samt smarta dräneringssystem för att leda bort vatten effektivt och minska risken för översvämningar.</p>
+            <p>Installation av snörasskydd för att förhindra snöskador och isbildning. Smarta dräneringssystem för att effektivt leda bort vatten och minska risken för översvämningar.</p>
           </div>
           <div className='safety-card'>
             <div><BsLadder className='safety-icon' /></div>
@@ -63,7 +63,7 @@ const TaksakerhetView = () => {
           <div className='safety-card'>
             <div><IoSnowSharp className='safety-icon' /></div>
             <h3>Säker snöröjning</h3>
-            <p>Professionell och säker snöröjning för att undvika tunga snömängder som kan orsaka skador eller kollaps.</p>
+            <p>Professionell snöröjning för att undvika tunga snömängder som kan orsaka kollaps av tak och/eller skador.</p>
           </div>
         </div>
       </div>
@@ -83,11 +83,11 @@ const TaksakerhetView = () => {
         )}
         <div className='safety-text'>
           <h2 className='sub-header container'>Välj långsiktig trygghet med Arosol</h2>
-          <p className='container'>Bristande taksäkerhet kan skada både taket och människor på marken. Stora mängder snö som faller obehindrat från ett högt tak kan få förödande konsekvenser och är ofta ett resultat av bristande taksäkerhet. Att säkra taket handlar om att bygga och underhålla det på ett sätt som minskar olycksriskerna och skyddar både byggnaden och dess invånare. <span className='new-paragraph'>Vi på Arosol strävar efter att inte bara tillgodose dina omedelbara behov, utan även att skapa en hållbar lösning som säkrar ditt tak och din trygghet. När du väljer Arosol får du mer än bara en pålitlig partner för taksäkerhet; du får en engagerad och professionell partner som behandlar ditt tak som om det vore vårt eget.</span></p>
+          <p className='container'>Bristande taksäkerhet kan skada både tak och människor. Stora mängder snö som faller obehindrat från ett högt tak kan få förödande konsekvenser och är ofta ett resultat av bristande taksäkerhet. Att säkra taket handla om att bygga och underhålla det på ett sätt som minskar riskerna för olyckor. <span className='new-paragraph'>Vi på Arosol strävar efter att inte bara tillgodose dina omedelbara behov, utan även att skapa en hållbar lösning som säkrar ditt tak och din trygghet. När du väljer Arosol får du mer än bara en pålitlig partner för taksäkerhet. Du får en engagerad och professionell partner som behandlar ditt tak som om det vore vårt eget.</span></p>
         </div>
       </div>
 
-      <ContactSection header='Vill du veta mer?' text='Är du intresserad av att lära dig mer om takskydd och vill ha ytterligare information? Tveka inte att kontakta oss! Arosol finns här för att besvara dina frågor och tillhandahålla all den information du behöver för att fatta välgrundade beslut om din taksäkerhet.' />
+      <ContactSection header='Vill du veta mer?' text='Är du intresserad av att lära dig mer om takskydd och vill ha ytterligare information? Tveka inte att kontakta oss. Arosol finns här för att besvara dina frågor och tillhandahålla all den information du behöver för att fatta välgrundade beslut om din taksäkerhet.' />
     </div>
   )
 }
