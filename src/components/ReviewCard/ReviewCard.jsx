d1a654b4bc8a35c1e84dd34a9dbbe710244fe4bb
Anna <annaelg@live.se>
2024-04-10 15:38:42 +0200

    Clean code and add picture to contact-card

--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -2,18 +2,20 @@ import { Link } from 'react-router-dom';
 import './ReviewCard.css'
 import { FaQuoteLeft } from 'react-icons/fa';
 
+import kontakt from '../../assets/kontakt.svg'
+
 
 const ReviewCard = ({ text, img, name, company, alt, contact }) => {
   return (
-    <div className='ReviewCard'>
+    <div className='ReviewCard relative d-flex-column'>
       {!contact ? 
         <>
           <FaQuoteLeft className='quote-icon' />
-          <div className='text-div'>
+          <div className='text-div d-flex-column'>
             <p className='text'>{text}</p>
           </div>
-          <div className='person-wrapper'>
-            <div className='quote-img-wrapper'>
+          <div className='person-wrapper d-flex'>
+            <div className='quote-img-wrapper img-cover'>
               <img src={img} alt={alt} />
             </div>
             <div className='name-company'>
@@ -23,9 +25,10 @@ const ReviewCard = ({ text, img, name, company, alt, contact }) => {
           </div>
         </>
         : 
-          <div className='text-div'>
-            <h3>Är du vår nästa kund?</h3>
+          <div className='text-div d-flex-column'>
+            <h2>Är du vår nästa kund?</h2>
             <Link to='contact' className='contact'><p>Kontakta oss</p></Link>
+            <img src={kontakt} alt='Kille som pratar i telefon' className='contact-img'/>
           </div>
     }
 
