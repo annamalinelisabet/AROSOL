aef586b164f558c4103e3c5ffd863caebaa36d78
Anna <annaelg@live.se>
2024-04-12 14:57:07 +0200

    Change text in not found

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
     <div className='NotFound container flex-center'>
         <div className='d-flex-column flex-center gap-1'>
             <h1>404</h1>
-            <p className='text-center'>Oops!<span className='line-break'>Ser ut som att solen inte riktigt hittade hit ännu, men vi jobbar på att ändra det.</span></p>       
+            <p className='text-center'>Oops!<span className='line-break'>Ser ut som att solen inte riktigt hittat hit ännu, men vi jobbar på att ändra det.</span></p>       
             <Link to='/'><Button text='Gå till startsida'/></Link>
         </div>
     </div>
